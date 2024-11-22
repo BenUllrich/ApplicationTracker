e84f31ce9c44bb7856beb103b8372b065d2d8314
Benjamin Ullrich <bullrich@stevens.edu>
2024-11-22 18:20:28 -0500

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,13 +1,5 @@
 import tkinter as tk
 
-def fahrenheit_to_celsius(ent_temperature,lbl_result):
-    """Convert the value for Fahrenheit to Celsius and insert the
-    result into lbl_result.
-    """
-    fahrenheit = ent_temperature.get()
-    celsius = (5 / 9) * (float(fahrenheit) - 32)
-    lbl_result["text"] = f"{round(celsius, 2)} \N{DEGREE CELSIUS}"
-
 def applicationsMenu():
     return
 
