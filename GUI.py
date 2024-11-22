634acf4593c57e07b2a39104953de475e19ca899
Benjamin Ullrich <bullrich@stevens.edu>
2024-11-22 18:19:32 -0500

    Add files via upload

---
+++ GUI.py
@@ -0,0 +1,57 @@
+import tkinter as tk
+
+def fahrenheit_to_celsius(ent_temperature,lbl_result):
+    """Convert the value for Fahrenheit to Celsius and insert the
+    result into lbl_result.
+    """
+    fahrenheit = ent_temperature.get()
+    celsius = (5 / 9) * (float(fahrenheit) - 32)
+    lbl_result["text"] = f"{round(celsius, 2)} \N{DEGREE CELSIUS}"
+
+def applicationsMenu():
+    return
+
+def settingsMenu():
+    return
+
+def filesMenu():
+    return
+
+def mainMenu():
+    # Set up the window
+    window = tk.Tk()
+    window.title("Main Menu")
+    window.resizable(width=False, height=False)
+    window.size()
+
+
+    btn_apps = tk.Button(
+        master=window,
+        text="Applications",
+        command=applicationsMenu(),
+        width=20,
+        height=5
+    )
+    btn_settings = tk.Button(
+        master=window,
+        text="Settings",
+        command=applicationsMenu(),
+        width=20,
+        height=5
+    )
+    btn_files = tk.Button(
+        master=window,
+        text="Files",
+        command=applicationsMenu(),
+        width=20,
+        height=5
+    )
+
+    # Set up the layout using the .grid() geometry manager
+    btn_apps.grid(row=0, column=1, pady=10, padx = 15)
+    btn_files.grid(row=1, column=1, pady=10, padx = 15)
+    btn_settings.grid(row=2, column=1, pady=10, padx = 15)
+
+
+    # Run the application
+    window.mainloop()
