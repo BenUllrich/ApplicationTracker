67b42c3e9e77ef6295d652834d1ce4030cb0a145
daveantala <dantala@stevens.edu>
2024-12-06 14:14:38 -0500

    Delete databaseManage.py

--- databaseManage.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('applications.db')
-c = conn.cursor()
-c.execute('''CREATE TABLE IF NOT EXISTS applications
-             (id INTEGER PRIMARY KEY, company TEXT, position TEXT, status TEXT, date_applied TEXT)''')
