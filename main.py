3e900cf9b56c755b0ca16f60ca55633e38385c8a
Benjamin Ullrich <bullrich@stevens.edu>
2024-12-08 17:08:07 -0500

    Fleshed out application updating, made small fixes and added docstrings

--- main.py
+++ main.py
@@ -6,10 +6,22 @@ import appSubmission
 import pandas as pd
 
 def updateApp(event):
-    if lb.curselection():
-           return
+    """
+    Updates the entry boxes depending on the selected application in the update tab
+    :param event:
+    :return:
+    """
+    if lbUpdate.curselection():
+        updateEntry.delete(0, END)
+        updateEntry.insert(0, apps.loc[lbUpdate.curselection()[0], 'Contact Person'])
+
 
 def refresh(arg=None):
+    """
+    Refreshes the listboxes throughout the interface
+    :param arg: The type of change that was made (newapp or update)
+    :return:
+    """
     global apps
     if arg == 'newapp':
         lb.delete(0, END)
@@ -22,8 +34,37 @@ def refresh(arg=None):
         for index, row in apps.iterrows():
             lb.insert(index, f"{row['Company']:}  ({row['Date']:>})")
             lbUpdate.insert(index, f"{row['Company']:}  ({row['Date']:>})")
+    if arg == 'update':
+        if lbUpdate.curselection():
+            index = lbUpdate.curselection()[0]
+            if editStatus_var.get() and editConn_var.get().strip():
+                new = Application.Application(apps.loc[index, 'Company'], apps.loc[index, 'Position'], apps.loc[index, 'Source'], editStatus_var.get(),
+                                        editConn_var.get())
+            elif editStatus_var.get():
+                new = Application.Application(apps.loc[index, 'Company'], apps.loc[index, 'Position'],
+                                              apps.loc[index, 'Source'], editStatus_var.get(),
+                                              apps.loc[index, 'Contact Person'])
+            elif editConn_var.get().strip():
+                new = Application.Application(apps.loc[index, 'Company'], apps.loc[index, 'Position'],
+                                              apps.loc[index, 'Source'], apps.loc[index, 'Status'],
+                                              editConn_var.get())
+            else: return
+            apps.drop(index, inplace = True)
+            apps.to_csv("processed_applications.csv", index = False)
+            lb.delete(0, END)
+            lbUpdate.delete(0, END)
+            appSubmission.submit_application(new)
+            apps = pd.read_csv("processed_applications.csv")
+            for i, row in apps.iterrows():
+                lb.insert(i, f"{row['Company']:}  ({row['Date']:>})")
+                lbUpdate.insert(i, f"{row['Company']:}  ({row['Date']:>})")
 
 def viewApp(event):
+    """
+    Updates the information fields when a new application is selected in the view tab
+    :param event:
+    :return:
+    """
     if lb.curselection():
            companyNameView.config(text="Company: " + apps.loc[lb.curselection()[0], 'Company'])
            jobTitleView.config(text="Position: " + apps.loc[lb.curselection()[0], 'Position'])
@@ -37,12 +78,13 @@ appSubmission.checkCSV("processed_applications.csv")
 
 # Set up the window
 window = Tk()
-#window.geometry("750x250")
+
 window.title("Application Tracker")
 window.resizable(width=False, height=False)
 window.size()
 window.config(bg="grey16")
 
+# A notebook is used to organize the functionality
 tabs = ttk.Notebook(window)
 tabs.pack(expand=1)
 
@@ -120,8 +162,23 @@ apps = pd.read_csv("processed_applications.csv")
 for index, row in apps.iterrows():
     lbUpdate.insert(index, f"{row['Company']:}  ({row['Date']:>})")
 
+Label(edit, text="New Connection:").grid(row=0, column=1)
+editConn_var = StringVar()
+
+updateEntry = Entry(edit, textvariable=editConn_var)
+updateEntry.grid(row=0, column=2)
+
+Label(edit, text="Update Application Status").grid(row=2, column=1)
+editStatus_var = StringVar()
+status_var.set("Options")
+OptionMenu(edit, editStatus_var, "Submitted", "Interviewing", "Offer Received", "Rejected").grid(row=2, column=2)
+
 lbUpdate.grid(row=0, column=0, rowspan=3)
 lbUpdate.bind('<<ListboxSelect>>', updateApp)
+Button(edit, text="Update",
+        command=lambda: refresh('update')).grid(row=5, column=0, columnspan=3)
+
+lbUpdate.select_set(0)
 
 
 # Run the application
